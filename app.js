6fb31f9e05aaacf4bcca3b449e40a3fdb7484259
Matthew Freeman <mjf3392@gmail.com>
2019-01-13 15:54:39 -0700

    New styles

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-fuction onReady() {
+function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
